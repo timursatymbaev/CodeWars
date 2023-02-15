e577c6f801fba588920ef74d2b0fa00eefd5965b
Timur <123528735+timursatymbaev@users.noreply.github.com>
2023-02-15 10:21:21 +0600

    Added new solution.

---
+++ JavaScript/6-kyu/Encrypt this!.js
@@ -0,0 +1,8 @@
+var encryptThis = function(text) {
+  return text.split(' ').map(e => {
+      if (e.length === 1) return e.charCodeAt(0);
+      if (e.length === 2) return `${e[0].charCodeAt(0)}${e[1]}`;
+      if (e.length === 3) return `${e[0].charCodeAt(0)}${e.slice(-1)}${e[1]}`;
+      if (e.length > 3) return `${e[0].charCodeAt(0)}${e.slice(-1)}${e.slice(2, -1)}${e[1]}`;
+    }).join(' ');
+}
