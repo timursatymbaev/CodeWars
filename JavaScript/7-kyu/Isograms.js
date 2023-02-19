60e80a78bfb99ff7717d69e20434570209552536
Timur <123528735+timursatymbaev@users.noreply.github.com>
2023-02-19 09:52:05 +0600

    Added new solutiob.

---
+++ JavaScript/7-kyu/Isograms.js
@@ -0,0 +1,15 @@
+function isIsogram(str) {
+  str = str.toLowerCase();
+  
+  let seen = new Set();
+  
+  for (let i = 0; i < str.length; i++) {
+    let char = str[i];
+    if (seen.has(char)) {
+      return false;
+    }
+    seen.add(char);
+  }
+  
+  return true;
+}
