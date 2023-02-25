9cfa120451dcb977fdb40cac46f18f137fd1bc07
Timur <123528735+timursatymbaev@users.noreply.github.com>
2023-02-25 13:10:49 +0600

    Added new solution.

---
+++ JavaScript/7-kyu/Highest and Lowest.js
@@ -0,0 +1,6 @@
+function highAndLow(numbers) {
+  const numArray = numbers.split(' ').map(Number);
+  const max = Math.max(...numArray);
+  const min = Math.min(...numArray);
+  return `${max} ${min}`;
+}
