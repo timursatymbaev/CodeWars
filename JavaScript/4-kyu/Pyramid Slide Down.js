e8fa84bd2a787e33ae8d37aa5251a671bdaae892
Timur <123528735+timursatymbaev@users.noreply.github.com>
2023-02-28 17:11:10 +0600

    Added new solution.

---
+++ JavaScript/4-kyu/Pyramid Slide Down.js
@@ -0,0 +1,9 @@
+function longestSlideDown(pyramid) {
+  for (let i = pyramid.length - 2; i >= 0; i--) {
+    for (let j = 0; j < pyramid[i].length; j++) {
+      pyramid[i][j] += Math.max(pyramid[i + 1][j], pyramid[i + 1][j + 1]);
+    }
+  }
+
+  return pyramid[0][0];
+}
