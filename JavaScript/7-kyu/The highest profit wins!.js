995f8d54db8663a2a3e51f19369e95099a8b2673
Timur <123528735+timursatymbaev@users.noreply.github.com>
2023-03-01 15:43:51 +0600

    Added new solution.

---
+++ JavaScript/7-kyu/The highest profit wins!.js
@@ -0,0 +1,12 @@
+function minMax(arr) {
+  let min = arr[0];
+  let max = arr[0];
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] < min) {
+      min = arr[i];
+    } else if (arr[i] > max) {
+      max = arr[i];
+    }
+  }
+  return [min, max];
+}
