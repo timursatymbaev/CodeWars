21f2dd1d508162bea74acb0d6be43f1152df6969
Timur <123528735+timursatymbaev@users.noreply.github.com>
2023-03-09 19:09:04 +0600

    Added new solution.

---
+++ JavaScript/7-kyu/Alternate case.js
@@ -0,0 +1,12 @@
+function alternateCase(str) {
+  let result = '';
+  for (let i = 0; i < str.length; i++) {
+    const char = str.charAt(i);
+    if (char === char.toUpperCase()) {
+      result += char.toLowerCase();
+    } else {
+      result += char.toUpperCase();
+    }
+  }
+  return result;
+}
