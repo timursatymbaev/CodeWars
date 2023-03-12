9ee9bd2f562f960d5919181a8d975f03498ab1ea
Timur <123528735+timursatymbaev@users.noreply.github.com>
2023-03-12 15:28:34 +0600

    Added new solution.

---
+++ JavaScript/8-kyu/Training JS #4: Basic data types--Array.js
@@ -0,0 +1,21 @@
+function getLength(arr){
+  return arr.length;
+}
+
+function getFirst(arr){
+  return arr[0];
+}
+
+function getLast(arr){
+  return arr[arr.length - 1];
+}
+
+function pushElement(arr){
+  arr.push(42);
+  return arr;
+}
+
+function popElement(arr){
+  arr.pop();
+  return arr;
+}
