e25f6bf2e5d1d29152df5d023a752ce1118727a3
Timur <123528735+timursatymbaev@users.noreply.github.com>
2023-03-14 08:25:25 +0600

    Added new solution.

---
+++ JavaScript/8-kyu/Grasshopper - Create the rooms.js
@@ -0,0 +1,17 @@
+const rooms = {
+  room1: {
+    name: "The Entrance",
+    description: "You are standing at the entrance of a mysterious castle.",
+    completed: false
+  },
+  room2: {
+    name: "The Library",
+    description: "You have entered a dusty old library, filled with books and scrolls.",
+    completed: false
+  },
+  room3: {
+    name: "The Crypt",
+    description: "You descend into a dark crypt, filled with the echoes of past battles.",
+    completed: false
+  }
+};
