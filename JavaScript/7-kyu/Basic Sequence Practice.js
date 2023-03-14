02fb88ec0dabe2ff5a8fef1f8f8c0babecfd3d84
Timur <123528735+timursatymbaev@users.noreply.github.com>
2023-03-14 08:24:23 +0600

    Added new solution.

---
+++ JavaScript/7-kyu/Basic Sequence Practice.js
@@ -0,0 +1,9 @@
+function sumOfN(n) {
+  var array = [];
+  array[0] = 0;
+  for (var i = 1; i < Math.abs(n)+1; i++) {
+    if (n > 0) array[i] = array[i-1]+i;
+    else array[i] = array[i-1]-i;
+  }
+  return array;
+};
